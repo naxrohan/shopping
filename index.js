fee67445ca9b39f6cd3f06b78c0580d2bd1d095d
naxrohan <rohan.jd@hotmail.com>
2022-07-06 12:32:32 +0100

    Corse exception added

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ mongoose.connect(
     console.log("Failed db connect..!!")
 });
 
+// enable CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
