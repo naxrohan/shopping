00c198800ba0675f58ad0f759f86357ebdba8fb4
naxrohan <rohan.jd@hotmail.com>
2022-08-01 16:32:59 +0100

    updated to api path

--- ecomsite_client/src/requestMethods.js
+++ ecomsite_client/src/requestMethods.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://shopsite1.herokuapp.com/api/";
+const BASE_URL = "http://localhost:5000/api/" // "https://shopsite1.herokuapp.com/api/";
 
 const storeData = JSON.parse(localStorage.getItem("persist:root"));
 const currentUser = storeData !== null 
-    ? storeData.user !== null ? JSON.parse(storeData.user).currentUser : null
+    ? storeData.user !== null ? storeData.currentUser : null
     : null;
 
 const B_TOKEN = currentUser !== null ? currentUser.accessToken : "xyz";
